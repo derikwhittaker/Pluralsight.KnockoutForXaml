a02f2a6f4180bb038afc97915852b2b0f0644712
Derik Whittaker <derik@graudo.com>
2013-05-09 04:28:43 -0400

    WIP out module 2

---
+++ Modules/02.working-with-input-controls/03.text-display-binding/coding.03/source-final/k4Xaml/ViewModels/MainViewModel.js
@@ -0,0 +1,8 @@
+var k4xaml;
+(function (k4xaml) {
+    var MainViewModel = (function () {
+        function MainViewModel() { }
+        return MainViewModel;
+    })();
+    k4xaml.MainViewModel = MainViewModel;    
+})(k4xaml || (k4xaml = {}));
